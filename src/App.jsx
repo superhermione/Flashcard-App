4ea8fe67d368dd8320ab871718cdbddee1724a32
Xinrui Hou <houxi@oregonstate.edu>
2023-09-26 00:31:41 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import './APP.CSS';
+import { useState } from 'react';
+
+//const [count, setCount] = useState(0);
+
+const App = () =>{
+  const flashCards = [{id:1, word:'熊猫', translation:'panda', color: '#a7eaaed9', img:'https://www.creativefabrica.com/wp-content/uploads/2023/05/19/Cute-Adorable-Baby-Panda-70054573-1.png'}, 
+  {id:2, word:'苹果', translation:'apple',color: '#a7eaaed9', img:'https://www.collinsdictionary.com/images/full/apple_158989157.jpg'},
+  {id:3, word:'手机', translation:'cellphone', color: '#a7eaaed9', img:'https://m.media-amazon.com/images/I/51-nHIqR7GL.jpg' },
+  {id:4, word:'玩具', translation:'toy',color: '#a7eaaed9', img:'https://cdn11.bigcommerce.com/s-ydriczk/images/stencil/1280x1280/products/89624/95788/Woody-Toy-Story-Cardboard-Cutout-Party-Decorations-buy-now-at-starstills__12633.1644939667.jpg?c=2'},
+  {id:5, word:'打篮球', translation:'play basketball', color:'#e6e26fd9', img:'https://img.freepik.com/premium-vector/vector-illustration-kids-playing-basketball_723224-732.jpg?w=2000'},
+  {id:6, word:'踢足球', translation:'play coccer', color:'#e6e26fd9', img:'https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/38244/schoolboys-soccer-football-clipart-xl.png'},
+  {id:7, word:'开车', translation:'drive',color:'#e6e26fd9', img:'https://static.vecteezy.com/system/resources/previews/008/386/524/original/cartoon-boy-driving-red-car-free-vector.jpg'},
+  {id:8, word:'美丽', translation:'beautiful', color:'#ea9177d9', img:'https://img.freepik.com/premium-photo/portrait-painting-beautiful-woman-multicolored-tones-abstract-picture-beautiful-girl_769803-2.jpg?w=2000'},
+  {id:9, word:'开心', translation:'happy',color:'#ea9177d9', img:'https://img.freepik.com/free-vector/happy-boy-jumping-colorful-background_1308-100697.jpg?w=2000'},
+  {id:10, word:'友善', translation:'friendly', color:'#ea9177d9', img:'https://pictures.abebooks.com/isbn/9781934277188-us.jpg'}];
+
+  const[currentCardId, setCurrentCardId] = useState(0);
+  const[showTranslation, setShowTranslation] = useState(0);
+  const currentFlashCard = flashCards[currentCardId];
+
+  const handleCardClick =() =>{
+    setShowTranslation(!showTranslation);
+  }
+
+  const handleNextCard =() =>{
+    const nextCardId = currentCardId + 1;
+    if(nextCardId < flashCards.length){
+      setCurrentCardId(nextCardId);
+      setShowTranslation(false);
+    }
+  }
+
+  const handlePreviousCard =() =>{
+    const previousCardId = currentCardId - 1;
+    if(previousCardId >= 0){
+      setCurrentCardId(previousCardId);
+      setShowTranslation(false);
+    }
+  }
+
+  return(
+    <div className='App'>
+      <h2>Welcome to the Chinese Learning Aid!</h2>
+      <h3>You will practice memorizing these vocab today!</h3>
+      <h4>Number of card: {flashCards.length}</h4>
+
+      <div className='card-container' onClick={handleCardClick} style={{ backgroundColor: currentFlashCard.color }}>
+        <div className={'flashcard' }>
+            <div className='card-text'>
+            {showTranslation ? (
+                <>
+                  <p>{currentFlashCard.translation}</p>
+                  <img src={currentFlashCard.img} alt="Translation" />
+                </>) : (
+                <p>{currentFlashCard.word}</p>
+              )}
+
+            </div>
+
+        </div>
+
+      </div>
+
+      <div className='card-changes'>
+        <button type="previous-card" onClick = {handlePreviousCard}>←</button>
+        <button type="next" onClick={handleNextCard}>⭢</button>
+      </div>
+
+    </div>
+
+  )
+}
+
+export default App
