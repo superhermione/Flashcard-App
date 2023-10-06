15f16c3587883412c50350fa67b3cf722d0f6e1e
Xinrui Hou <houxi@oregonstate.edu>
2023-10-06 16:54:47 -0500

    Update part II features and README

--- README.md
+++ README.md
@@ -1,4 +1,46 @@
-# Web Development Project 2 - *Language Learning Aid*
+# Web Development Project 3 - *Language Learning Aid* Part II
+
+Submitted by: **Xinrui Hou**
+
+This web app: **This application is designed for new language learners to practice Chinese words on a daily basis. It has visuals and translation. The part II added a new feature for users to practice typing the meaning. The user will get a bollean signal based on the results and the correct answers will be recorded as a correct streak. The third feature is that the flashcards can be shuffled. The pronunciation feature will be added soon.**
+
+Time spent: **3** hours spent in total
+
+## Required Features
+
+The following **required** functionality is completed:
+
+- [x] **The user can enter their guess in a box before seeing the flipside of the card**
+- [x] **Clicking on a submit button shows visual feedback about whether the answer was correct or incorrect**
+- [x] **A back button is displayed on the card and can be used to return to the previous card in a set sequence**
+- [x] **A next button is displayed on the card and can be used to navigate to the next card in a set sequence**
+
+The following **optional** features are implemented:
+
+- [x] A shuffle button is used to randomize the order of the cards
+- [x] A user's answer may be counted as correct even when it is slightly different from the target answer
+- [x] A counter displays the user's current and longest streak of correct responses
+- [ ] A user can mark a card that they have mastered and have it removed from the pool of answers as well as added to a list of mastered cards
+
+The following **additional** features are implemented:
+
+* [ ] List anything else that you added to improve the site's functionality!
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented user stories:
+
+<img src='./assets/walkthrough2.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+
+GIF created with gifcap
+
+
+## Notes
+
+Describe any challenges encountered while building the app.
+
+
+# Web Development Project 2 - *Language Learning Aid* Part I
 
 Submitted by: **Xinrui Hou**
 
@@ -25,31 +67,3 @@ The following **optional** features are implemented:
 The following **additional** features are implemented:
 
 * [ ] Working on adding pronunciation to the card as well as fully flip the card. Not fully implemented. 
-
-## Video Walkthrough
-
-Here's a walkthrough of implemented required features:
-
-<img src='./assets/walkthrough.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
-
-GIF created with gifcap
-
-## Notes
-
-To achieve the additional feature which is to flip the card is a ongoing process. 
-
-## License
-
-    Copyright [2023] [Xinrui Hou]
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-        http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,126 @@
 import './APP.CSS';
 import { useState } from 'react';
 
-//const [count, setCount] = useState(0);
+const App = () => {
 
-const App = () =>{
-  const flashCards = [{id:1, word:'熊猫', translation:'panda', color: '#a7eaaed9', img:'https://www.creativefabrica.com/wp-content/uploads/2023/05/19/Cute-Adorable-Baby-Panda-70054573-1.png'}, 
-  {id:2, word:'苹果', translation:'apple',color: '#a7eaaed9', img:'https://www.collinsdictionary.com/images/full/apple_158989157.jpg'},
-  {id:3, word:'手机', translation:'cellphone', color: '#a7eaaed9', img:'https://m.media-amazon.com/images/I/51-nHIqR7GL.jpg' },
-  {id:4, word:'玩具', translation:'toy',color: '#a7eaaed9', img:'https://cdn11.bigcommerce.com/s-ydriczk/images/stencil/1280x1280/products/89624/95788/Woody-Toy-Story-Cardboard-Cutout-Party-Decorations-buy-now-at-starstills__12633.1644939667.jpg?c=2'},
-  {id:5, word:'打篮球', translation:'play basketball', color:'#e6e26fd9', img:'https://img.freepik.com/premium-vector/vector-illustration-kids-playing-basketball_723224-732.jpg?w=2000'},
-  {id:6, word:'踢足球', translation:'play coccer', color:'#e6e26fd9', img:'https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/38244/schoolboys-soccer-football-clipart-xl.png'},
-  {id:7, word:'开车', translation:'drive',color:'#e6e26fd9', img:'https://static.vecteezy.com/system/resources/previews/008/386/524/original/cartoon-boy-driving-red-car-free-vector.jpg'},
-  {id:8, word:'美丽', translation:'beautiful', color:'#ea9177d9', img:'https://img.freepik.com/premium-photo/portrait-painting-beautiful-woman-multicolored-tones-abstract-picture-beautiful-girl_769803-2.jpg?w=2000'},
-  {id:9, word:'开心', translation:'happy',color:'#ea9177d9', img:'https://img.freepik.com/free-vector/happy-boy-jumping-colorful-background_1308-100697.jpg?w=2000'},
-  {id:10, word:'友善', translation:'friendly', color:'#ea9177d9', img:'https://pictures.abebooks.com/isbn/9781934277188-us.jpg'}];
+    // Make flashCards to be useState so that it can be shuffled later
+    const [flashCards, setFlashCards] = useState(
+      [{id:1, word:'熊猫', translation:'panda', color: '#a7eaaed9', img:'https://www.creativefabrica.com/wp-content/uploads/2023/05/19/Cute-Adorable-Baby-Panda-70054573-1.png'}, 
+      {id:2, word:'苹果', translation:'apple',color: '#a7eaaed9', img:'https://www.collinsdictionary.com/images/full/apple_158989157.jpg'},
+      {id:3, word:'手机', translation:'cellphone', color: '#a7eaaed9', img:'https://m.media-amazon.com/images/I/51-nHIqR7GL.jpg' },
+      {id:4, word:'玩具', translation:'toy',color: '#a7eaaed9', img:'https://cdn11.bigcommerce.com/s-ydriczk/images/stencil/1280x1280/products/89624/95788/Woody-Toy-Story-Cardboard-Cutout-Party-Decorations-buy-now-at-starstills__12633.1644939667.jpg?c=2'},
+      {id:5, word:'打篮球', translation:'play basketball', color:'#e6e26fd9', img:'https://img.freepik.com/premium-vector/vector-illustration-kids-playing-basketball_723224-732.jpg?w=2000'},
+      {id:6, word:'踢足球', translation:'play soccer', color:'#e6e26fd9', img:'https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/38244/schoolboys-soccer-football-clipart-xl.png'},
+      {id:7, word:'开车', translation:'drive',color:'#e6e26fd9', img:'https://static.vecteezy.com/system/resources/previews/008/386/524/original/cartoon-boy-driving-red-car-free-vector.jpg'},
+      {id:8, word:'美丽', translation:'beautiful', color:'#ea9177d9', img:'https://img.freepik.com/premium-photo/portrait-painting-beautiful-woman-multicolored-tones-abstract-picture-beautiful-girl_769803-2.jpg?w=2000'},
+      {id:9, word:'开心', translation:'happy',color:'#ea9177d9', img:'https://img.freepik.com/free-vector/happy-boy-jumping-colorful-background_1308-100697.jpg?w=2000'},
+      {id:10, word:'友善', translation:'friendly', color:'#ea9177d9', img:'https://pictures.abebooks.com/isbn/9781934277188-us.jpg'}]
+    );
 
-  const[currentCardId, setCurrentCardId] = useState(0);
-  const[showTranslation, setShowTranslation] = useState(0);
-  const currentFlashCard = flashCards[currentCardId];
+    const [currentCardId, setCurrentCardId] = useState(0);
+    const [showTranslation, setShowTranslation] = useState(0);
+    const [userAnswer, setUserAnswer] = useState('');
+    const [showResult, setShowResult] = useState(false);
+    const [isCorrect, setIsCorrect] = useState(false);
+    const [correctStreak, setCorrectStreak] = useState(0);
 
-  const handleCardClick =() =>{
-    setShowTranslation(!showTranslation);
-  }
+    const currentFlashCard = flashCards[currentCardId];
 
-  const handleNextCard =() =>{
-    const nextCardId = currentCardId + 1;
-    if(nextCardId < flashCards.length){
-      setCurrentCardId(nextCardId);
-      setShowTranslation(false);
+    // handleCardClick to show translation
+    const handleCardClick = () => {
+        setShowTranslation(!showTranslation);
     }
-  }
 
-  const handlePreviousCard =() =>{
-    const previousCardId = currentCardId - 1;
-    if(previousCardId >= 0){
-      setCurrentCardId(previousCardId);
-      setShowTranslation(false);
+    // handleNextCard to show next
+    const handleNextCard = () => {
+        const nextCardId = currentCardId + 1;
+        if (nextCardId < flashCards.length) {
+            setCurrentCardId(nextCardId);
+            setShowTranslation(false);
+            setUserAnswer('');
+            setShowResult(false);
+        }
+    };
+
+  // handlePreviousCard to go to previous card
+    const handlePreviousCard = () => {
+        const previousCardId = currentCardId - 1;
+        if (previousCardId >= 0) {
+            setCurrentCardId(previousCardId);
+            setShowTranslation(false);
+            setShowResult(false);
+        }
+    };
+
+  // handleSubmit to show user's resluts and streak
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const currentCard = flashCards[currentCardId];
+        if (userAnswer === currentCard.translation) {
+            setIsCorrect(true);
+            setCorrectStreak(correctStreak + 1);
+        } else {
+            setIsCorrect(false);
+        }
+        setShowResult(true);
+    };
+
+    //Implement Fisher-Yates shuffle algorithm
+    const shuffleArray = (array) => {
+        let currentIndex = array.length, randomIndex;
+        while (currentIndex !== 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+            [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+        }
+        return array;
     }
-  }
 
-  return(
-    <div className='App'>
-      <h2>Welcome to the Chinese Learning Aid!</h2>
-      <h3>You will practice memorizing these vocab today!</h3>
-      <h4>Number of card: {flashCards.length}</h4>
+    // handleShuffleCards to shuffle the flashcards
+    const handleShuffleCards = () => {
+        const shuffledCards = shuffleArray([...flashCards]);
+        setFlashCards(shuffledCards);
+    };
 
-      <div className='card-container' onClick={handleCardClick} style={{ backgroundColor: currentFlashCard.color }}>
-        <div className={'flashcard' }>
-            <div className='card-text'>
-            {showTranslation ? (
-                <>
-                  <p>{currentFlashCard.translation}</p>
-                  <img src={currentFlashCard.img} alt="Translation" />
-                </>) : (
-                <p>{currentFlashCard.word}</p>
-              )}
-
+    return (
+        <div className='App'>
+            <h2>Welcome to the Chinese Learning Aid!</h2>
+            <h3>You will practice memorizing these vocab today!</h3>
+            <h4>Number of card: {flashCards.length}
+                <button type='shuffle' onClick={handleShuffleCards}>Shuffle Cards</button>
+            </h4>
+            
+            <div className='card-container' onClick={handleCardClick} style={{ backgroundColor: currentFlashCard.color }}>
+                <div className={'flashcard' }>
+                    <div className='card-text'>
+                        {showTranslation ? (
+                            <>
+                                <p>{currentFlashCard.translation}</p>
+                                <img src={currentFlashCard.img} alt="Translation" />
+                            </>
+                        ) : (
+                            <p>{currentFlashCard.word}</p>
+                        )}
+                    </div>
+                </div>
             </div>
-
+            <div className='card-changes'>
+                <button type="previous-card" onClick={handlePreviousCard}>←</button>
+                <button type="next" onClick={handleNextCard}>⭢</button>
+            </div>
+            <form onSubmit={handleSubmit}>
+                Your answer:
+                <input type='text' value={userAnswer} onChange={(e) => setUserAnswer(e.target.value)} />
+                <button type='submit'>Submit</button>
+            </form>
+            {showResult && (
+                <div className='result' style={{ backgroundColor: isCorrect ? 'green' : 'red' }}>
+                    {isCorrect ? 'Correct!' : "Incorrect!"}
+                </div>
+            )}
+            <h4>Current Correct Streak: {correctStreak}</h4>
         </div>
-
-      </div>
-
-      <div className='card-changes'>
-        <button type="previous-card" onClick = {handlePreviousCard}>←</button>
-        <button type="next" onClick={handleNextCard}>⭢</button>
-      </div>
-
-    </div>
-
-  )
+    )
 }
 
-export default App
+export default App;
